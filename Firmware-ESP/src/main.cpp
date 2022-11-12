b6459039973f569fc2caeea9415d701e56d958c6
vdgaete <vdgaete@hotmail.com>
2022-11-12 20:03:02 -0300

    feat: ticker

--- Firmware-ESP/src/main.cpp
+++ Firmware-ESP/src/main.cpp
@@ -4,45 +4,55 @@
 #include "MAX30100_PulseOximeter.h"
 #include <ArduinoJson.h>
 #include <TaskScheduler.h>
+#include <Ticker.h>
+
+/* Dividers:
+		TIM_DIV1 = 0,   //80MHz (80 ticks/us - 104857.588 us max)
+		TIM_DIV16 = 1,  //5MHz (5 ticks/us - 1677721.4 us max)
+		TIM_DIV256 = 3  //312.5Khz (1 tick = 3.2us - 26843542.4 us max)
+	Reloads:
+		TIM_SINGLE	0 //on interrupt routine you need to write a new value to start the timer again
+		TIM_LOOP	1 //on interrupt the counter will start with the same value again
+	*/
+
+Ticker timer1;
+volatile int flag_send;
 
 
-#define SEND_RAW_DATA 0
 #define MSG_BUFFER_SIZE	100
 #define REPORTING_PERIOD 5000
-#define B_PERIOD 100
-#define B_ITERATIONS 1
-#define LOOP_PERIOD 4000
-#define LOOP_ITERATIONS 1
 
 
+/*
 Scheduler ts;
 
 void task_beat_callback();
-void task_loop_callback();
-void task_beat_onDisable();
+//void task_loop_callback();
 void task_send_callback();
-void task_send_onDisable();
+
 
 Task beat(B_PERIOD * TASK_MILLISECOND, B_ITERATIONS,&task_beat_callback,&ts,true);
-Task mqttLoop(LOOP_PERIOD * TASK_MILLISECOND, LOOP_ITERATIONS,&task_loop_callback,&ts,true);
+//Task mqttLoop(LOOP_PERIOD * TASK_MILLISECOND, LOOP_ITERATIONS,&task_loop_callback,&ts,true);
 Task dataSend(LOOP_PERIOD * TASK_MILLISECOND, LOOP_ITERATIONS,&task_send_callback,&ts,true);
-//uint32_t tsLastReport = 0;
-PulseOximeter pox;
 
+
+*/
+
+
+uint32_t tsLastReport = 0;
 const char* ssid = "MERCUSYS";
 const char* password = "ic123456";
 const char* mqtt_server = "192.168.1.100";
 const int mqtt_port = 1883;
 
-DynamicJsonDocument doc(1024);
-char buff[1000];
+DynamicJsonDocument doc(255);
+char buff[250];
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 unsigned long lastMsg = 0;
 
-char msg[MSG_BUFFER_SIZE];
 int value = 0;
 int count = 0;
 
@@ -67,9 +77,10 @@ void reconnect() {
     }
   }
 }
+/* 
 
 void task_beat_callback(){
-  Serial.print("Beat? ... ");
+  Serial.print("Beat ...");
   pox.update();
 }
 
@@ -82,6 +93,11 @@ void task_loop_callback(){
 }
 
 void task_send_callback(){
+  if (!client.connected()) {
+      reconnect();
+    }
+  client.loop();
+  Serial.println("loop");
   if (count>5){
     if (!client.connected()) {
       reconnect();
@@ -95,6 +111,7 @@ void task_send_callback(){
   }
 
 }
+*/
 
 void onBeatDetected() {
     count++;
@@ -132,36 +149,55 @@ void callback(char* topic, byte* payload, unsigned int length) {
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
-  Serial.println();
 }
 
+PulseOximeter sensor;
 
+void ICACHE_RAM_ATTR onTime() {
+	sensor.update();
+  
+}
 
 void setup() {
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
+
+  
   // Config BioHub
-  if (!pox.begin()) {
+  if (!sensor.begin()) {
         Serial.println("FAILED");
         for(;;);
     } else {
         Serial.println("SUCCESS");
     }
-
-  pox.setIRLedCurrent(MAX30100_LED_CURR_11MA);
-  pox.setOnBeatDetectedCallback(onBeatDetected);
+  sensor.setIRLedCurrent(MAX30100_LED_CURR_11MA);
+  sensor.setOnBeatDetectedCallback(onBeatDetected);
   // End Config Bio
-
+  timer1_attachInterrupt(onTime); // Add ISR Function
+	timer1_enable(TIM_DIV16, TIM_EDGE, TIM_LOOP);
+  timer1_write(500000);
 }
 
 void loop() {
-  
-  ts.execute();
-
+  yield();
+  if((millis()-tsLastReport)>REPORTING_PERIOD){ 
+    if (count>3){
+    if (!client.connected()) {
+      reconnect();
+    }
+    doc["heartrate"] = sensor.getHeartRate();
+    doc["oxygen"] = sensor.getSpO2();
+    serializeJson(doc,buff);
+    Serial.println(buff);
+    client.publish("Device/BO",buff);
+    client.loop();
+    count=0;
+    }
+  }
   /*
-  if (count>6||(millis() - tsLastReport)>REPORTING_PERIOD_MS) {
+  if (count>5&&(millis()-tsLastReport)>REPORTING_PERIOD) {
     if (!client.connected()) {
       reconnect();
     }
@@ -176,3 +212,4 @@ void loop() {
   }
   */
 }
+
